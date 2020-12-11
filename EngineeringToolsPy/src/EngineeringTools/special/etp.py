c2bfbaf8bb8d363595d31b2808b04d178534bcc5
Martin Hochwallner <marthoch@users.noreply.github.com>
2020-12-11 22:30:04 +0100

    added ETP Express, work left to do

--- EngineeringToolsPy/src/EngineeringTools/special/etp.py
+++ EngineeringToolsPy/src/EngineeringTools/special/etp.py
@@ -95,4 +95,83 @@ p0:             {me.p0}
         force = ETQ.Force(force)
         return ETQ.Pressure(force / (self.active_area*self.friction_coefficient_design)  + self.p0.uval)
 
+
+
+class ETP_EXPRESS:
+    """
+    https://etp.se/sites/default/files_two/ETP-EXPRESS-PRODUCT-SHEET.pdf
+    """
+    filename = os.path.join(os.path.dirname(__file__), r'ETP_EXPRESS.txt')
+    database = pd.read_csv(filename, skiprows=4, sep=r'\s*;\s*', index_col=0, engine='python')
+
+    def __init__(self, id=None, d=None):
+
+        if id:
+            dataset = self.database.loc[id]
+        elif d:
+            dataset = self.database[self.database['d'] == d].iloc[0]
+        else:
+            raise Exception()
+        self.dataset = dataset
+        self.name   = dataset.name
+        self.length = ETQ.Distance(dataset['L'], 'mm')
+        self.lengthTotal = ETQ.Distance(dataset['L1'], 'mm')
+        self.inner_diameter = ETQ.Distance(dataset['d'], 'mm')
+        self.outer_diameter = ETQ.Distance(dataset['D'], 'mm')
+        self.torque = ETQ.Torque(dataset['T'], 'N.m')
+#        self.lst_force = [ETQ.Force(dataset['F1'], 'kN'), ETQ.Force(dataset['F2'], 'kN')]
+#        self.lst_pressure = [ETQ.Pressure(350., 'bar'), ETQ.Pressure(450., 'bar')]
+        self.friction_coefficient_design = 0.1
+ 
+        self.friction_coefficient = r_[0.05, 0.1, 0.2] # steel on steel, oil: 0.05 .. 0.1, 0.2 without oil
+#        self.pressure_offset = r_[5.0, 10., 20.]*1e5 # Pa, pressure where clamping starts
+ 
+        # add the zero point
+        # TODO: this is not perfect, more data needed
+#        self.lst_force.append(ETQ.Force(0., 'N'))
+#        self.lst_pressure.append(ETQ.Pressure(1., 'bar'))
+#        self.calc_param()
+#        del self.lst_force[-1]
+#        del self.lst_pressure[-1]
+
+
+#     def calc_param(self):
+#         p = r_[[x.value for x in self.lst_pressure]]
+#         F = r_[[x.value for x in self.lst_force]]
+#         slope, intercept, _, _, _ = spstats.linregress(p, F)
+#         self.active_area = ETQ.Area(slope / self.friction_coefficient_design, 'm2')
+#         self.p0 = ETQ.Pressure(-intercept / slope, 'Pa')
+
+
+    def __str__(self):
+#        pf = ""
+#        for p, f in zip(self.lst_pressure, self.lst_force):
+#            pf += '{} {}\n'.format(p, f)
+        return """ETP ClampingElement
+name:           ETP_EXPRESS {me.name}
+rod diameter:   {me.inner_diameter}
+outer diameter: {me.outer_diameter}
+length:         {me.length}
+lengthTotal:    {me.lengthTotal}
+Torque:         {me.torque}
+ 
+""".format(me=self)
+
+    __repr__ = __str__
+
+
+
+    @classmethod
+    def list_all(cls):
+        #database = pd.read_csv(cls.filename, skiprows=3, sep=r'\s*;\s*', index_col=0, engine='python')
+        return cls.database #.index()
+
+#     def clamp_force(self, pressure):
+#         pressure = ETQ.Pressure(pressure)
+#         return ETQ.Force(self.active_area*self.friction_coefficient_design* max(pressure - self.p0, ETQ.Pressure(0.0, 'Pa')))
+# 
+#     def clamp_pressure_needed(self, force):
+#         force = ETQ.Force(force)
+#         return ETQ.Pressure(force / (self.active_area*self.friction_coefficient_design)  + self.p0.uval)
+
 #eof
